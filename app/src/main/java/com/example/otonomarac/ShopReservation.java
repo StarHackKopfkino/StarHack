beb843f96c60113602ea13bbb6afaa9b831ef8f4
yemre <y.emrezobi@gmail.com>
2021-12-12 09:07:36 +0300

    Add toast on successful transaction

--- app/src/main/java/com/example/otonomarac/ShopReservation.java
+++ app/src/main/java/com/example/otonomarac/ShopReservation.java
@@ -2,6 +2,7 @@ package com.example.otonomarac;
 
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -145,5 +147,12 @@ public class ShopReservation extends AppCompatActivity
         Reservation newReservation = new Reservation(reservations.size(), days, months, years, hours, minutes, duration, Storage.getInstance().getShopById(shopId));
 
         reservations.add(newReservation);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Transaction Successful";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
